5c64f757f18dc347242e03158e9709961df39fe6
Diego <arnedoav@gmail.com>
2022-03-02 12:48:15 +0100

    entrega ejercicio enigma

---
+++ enigma/app.js
@@ -0,0 +1,47 @@
+var plainAlphabet = "abcdefghijklmnopqrstuvwxyz:()!¡,'";
+var encryptAlphabet = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm";
+
+
+var encryptText = "";
+var decryptText = "";
+
+function ecrypt() {
+    var text = document.getElementById("text").value;
+    for (var i = 0; i < text.length; i++) {
+        var lowerText = text.toLowerCase(text);
+        var letter = false;
+        for (var x = 0; x < plainAlphabet.length; x++) {
+            if (lowerText[i] == plainAlphabet[x]) {
+                encryptText = encryptText + encryptAlphabet[x];
+                var letter = true;
+            }
+        }
+        if (letter == false) {
+            encryptText = encryptText + lowerText[i];
+        }
+    }
+    console.log(encryptText);
+    document.getElementById("encryptArea").value = encryptText;
+}
+
+function decrypt() {
+    var encryptText = document.getElementById("encryptArea").value;
+    for (var i = 0; i < encryptText.length; i++) {
+        var letter = false;
+        for (var x = 0; x < encryptAlphabet.length; x++) {
+            if (encryptText[i] == encryptAlphabet[x]) {
+                decryptText = decryptText + plainAlphabet[x];
+                var letter = true
+            }
+        }
+        if (letter == false) {
+            decryptText = decryptText + encryptText[i];
+        }
+    }
+    console.log(decryptText);
+    document.getElementById("text").value = decryptText;
+}
+
+
+
+
